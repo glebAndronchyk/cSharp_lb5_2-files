92808600b2a3676f3b94ba9b4e81ce7644ab1289
Gleb <gleb.andronchyk@gmail.com>
2023-04-30 16:19:49 +0300

    Reading file. Creating list of students. Checkers for struct data

---
+++ LB5_2/Checkers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LB5_2
+{
+    public class Checkers
+    {
+        public static char CheckSex(string sex)
+        {
+            char sex_ = Char.Parse(sex);
+            switch (sex_)
+            {
+                case 'M':
+                case 'М':
+                case 'Ч':
+                case 'F':
+                case 'Ж':
+                    return sex_;
+                default:
+                    throw new Exception($"Unknown gender: {sex}");
+            }
+        }
+
+        public static string CheckDate(string date)
+        {
+            string[] date_ = date.Split('.');
+            if (date_.Length < 3 || date_.Length > 3)
+            {
+                throw new Exception($"Invalid date of birth: {date}");
+            }
+
+            if (date_[0].Length == 2 && date_[1].Length == 2 && date_[2].Length == 4)
+            {
+                return date;
+            }
+            
+            throw new Exception($"Invalid date of birth: {date}");
+        }
+
+        public static char CheckMark(string mark)
+        {
+            char mark_ = Char.Parse(mark);
+
+            if (mark == "-" || int.Parse(mark) >= 2 && int.Parse(mark) <= 5)
+            {
+                return mark_;
+            }
+            
+            throw new Exception($"Invalid mark: {mark}");
+        }
+
+        public static int CheckScholarship(string scholarship)
+        {
+            try
+            {
+                int scholarship_ = int.Parse(scholarship);
+                if (scholarship_ >= 1234 && scholarship_ <= 4321 || scholarship_ == 0)
+                {
+                    return scholarship_;
+                }
+
+                throw new Exception($"Scholarship is invalid: {scholarship}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 0;
+            }
+        }
+    }
+}
---
+++ LB5_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LB5_2
+{
+    partial class Program
+    {
+        static List<Student> ReadData(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string studentInfo;
+            List<Student> students = new List<Student>();
+            
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    studentInfo = sr.ReadLine();
+                    Student student = new Student(studentInfo);
+                    students.Add(student);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Failed to read line {fileName}:\n" + e.Message);
+                throw;
+            }
+
+            return students;
+        }
+
+        static void runMenu(List<Student> studs)
+        {
+            Console.WriteLine(studs[0].firstName);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> studs = ReadData("input.txt");
+            runMenu(studs);
+        }
+    }
+}
